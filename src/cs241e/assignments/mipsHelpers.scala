5e3923a4cc592f58b576a43f233644cdba81baf9
Balaji Leninrajan <bleninrajan@uwaterloo.ca>
2024-10-20 19:06:42 -0400

    improved mips helper functions

--- src/cs241e/assignments/mipsHelpers.scala
+++ src/cs241e/assignments/mipsHelpers.scala
@@ -6,6 +6,7 @@ import ProgramRepresentation.*
 import CodeBuilders.*
 
 object mipsHelpers {
+  // get element at index from array
   def getArrayElem(array: Variable, index: Variable) = block(
     binOp(
       getVar(array),
@@ -19,23 +20,57 @@ object mipsHelpers {
     LW(Reg.result, 0, Reg.result)
   )
 
+  // get element at index from array
+  def getArrayElem(array: Variable, index: Reg) = block(
+    binOp(
+      getVar(array),
+      plus,
+      binOp(
+        getReg(index),
+        times,
+        const(4)
+      )
+    ),
+    LW(Reg.result, 0, Reg.result)
+  )
+
+  // get element at index from array
+  def getArrayElem(array: Reg, index: Reg) = block(
+    binOp(
+      getReg(array),
+      plus,
+      binOp(
+        getReg(index),
+        times,
+        const(4)
+      )
+    ),
+    LW(Reg.result, 0, Reg.result)
+  )
+
+  // stores constant in int in Reg.result
   def const(i: Int) = block(
     LIS(Reg.result),
     Word(encodeSigned(i))
   )
 
+  // stores value of r in Reg.result
   def getReg(r: Reg) = ADD(Reg.result, Reg.zero, r)
 
+  // stores value of v in Reg.result
   def getVar(v: Variable) = read(Reg.result, v)
 
+  // prints r (mutates Reg.scratch)
   def printReg(r: Reg) = block(
     LIS(Reg.scratch),
     CPU.printAddr,
     SW(r, 0, Reg.scratch)
   )
 
+  // assigns b to a
   def move(a: Reg, b: Reg) = ADD(a, b, Reg.zero)
 
+  // pushes r onto the stack (mutates Reg.scratch)
   def stackPush(r: Reg) = block(
     LIS(Reg.scratch),
     Word(encodeSigned(4)),
@@ -43,14 +78,17 @@ object mipsHelpers {
     SW(r, 0, Reg.stackPointer)
   )
 
+  // gets top element from stack
   def stackTop(r: Reg) = LW(r, 0, Reg.stackPointer)
 
+  // pops top element from stack (mutates Reg.scratch)
   def stackPop() = block(
     LIS(Reg.scratch),
     Word(encodeSigned(4)),
     ADD(Reg.stackPointer, Reg.stackPointer, Reg.scratch)
   )
 
+  // pops and returns top element from stack (mutates Reg.scratch)
   def stackPop(r: Reg) = block(
     stackTop(r),
     LIS(Reg.scratch),
